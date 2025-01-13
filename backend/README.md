d60c262702f1bb112f11d445908fb674e7a892e4
Lucas Boucher <contact@lucasboucher.fr>
2025-01-13 18:13:11 +0100

    feat: first endpoint to get a wallet

--- backend/README.md
+++ backend/README.md
@@ -2,6 +2,8 @@
 
 > Projet initialisé avec [Bun](https://bun.sh)
 
+# Lancer le projet
+
 Installer les dépendances :
 
 ```bash
@@ -12,4 +14,18 @@ Lancer le serveur :
 
 ```bash
 bun dev
+```
+
+Générer le client Prisma :
+
+```bash
+bunx prisma generate
+```
+
+# Développer le projet
+
+Pour créer une nouvelle migration :
+
+```bash
+bunx prisma migrate dev
 ```
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,25 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const port = 8080;
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/history", async (req, res) => {
+  const walletHistory = await prisma.walletHistory.findMany({
+    where: {
+      walletId: 1,
+    },
+    include: {
+      currency: true,
+      wallet: true,
+    },
+  });
+  res.json(walletHistory);
 });
 
 app.listen(port, () => {
