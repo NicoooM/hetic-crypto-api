f9b0b320229b09b64c5711eec3b8cf631ef62e9a
Julian Laballe <julian.laballe@unlikely.technology>
2025-01-14 15:48:01 +0100

    feat: add authRouter

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,23 @@
+import { AuthController } from "controllers/auth.controller";
+import express from "express";
+
+const authRouter = express.Router();
+const authController = new AuthController();
+
+// router.post("/auth/login", authController.login);
+// router.post("/auth/logout", authController.logout);
+
+// router.post("/auth/refresh", authController.refresh);
+
+// authRouter.get("/test", (req, res) => {
+//   res.send("Hello World! From Auth");
+// });
+
+// authRouter.get("verify-email/:token", authController.verifyEmail);
+
+authRouter.post("/register", authController.register);
+authRouter.get("/verify-email/:token", (req, res) => {
+  const token = req.params.token;
+  res.send(`Hello World! From Auth ${token}`);
+});
+export { authRouter };
