05719024bbc958c9353669057e3c0cb6ed4ffa6a
Lucas Boucher <contact@lucasboucher.fr>
2025-01-15 16:11:43 +0100

    feat: portfolio route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -90,10 +90,10 @@ app.get("/profile", async (req, res) => {
 });
 
 app.post("/wallet", async (req, res) => {
-  const { address, userId, title } = req.body;
+  const { address, title } = req.body;
 
-  if (!address || !userId || !title) {
-    res.status(400).json({ error: "Address and userId are required" });
+  if (!address || !title) {
+    res.status(400).json({ error: "Address and title are required" });
   }
 
   try {
@@ -177,6 +177,75 @@ app.delete("/wallet/:id", async (req, res) => {
       res.status(500).json({ error: "Internal server error" });
     }
   }
+});
+
+app.get("/portfolio/:id", async (req, res) => {
+  const walletId = parseInt(req.params.id, 10);
+
+  // Get wallet address
+  const wallet = await prisma.wallet.findUnique({
+    where: {
+      id: walletId,
+    },
+    select: {
+      address: true,
+    },
+  });
+  const walletAddress = wallet?.address;
+
+  // Get last wallet value history
+  const walletHistory = await prisma.walletHistory.findFirst({
+    where: {
+      walletId: walletId,
+    },
+    select: {
+      value: true,
+    },
+  });
+  const lastWalletHistoryValue = walletHistory?.value;
+
+  // Allocation
+  const allocation = 1;
+
+  // Price
+  const price = await fetch(
+    "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=EUR"
+  );
+  const priceResponse = await price.json();
+  const priceData = priceResponse.EUR;
+
+  // Daily price
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  const timestamp = Math.floor(yesterday.getTime() / 1000);
+  const dailyPriceFetch = await fetch(
+    `https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=EUR&ts=${timestamp}`
+  );
+  const dailyPriceResponse = await dailyPriceFetch.json();
+  const dailyPriceData = dailyPriceResponse.ETH.EUR;
+  const dailyPrice = ((priceData - dailyPriceData) / dailyPriceData) * 100;
+
+  // Value
+  const valueFetch = await fetch(
+    `https://api.etherscan.io/api?module=account&action=balance&address=${walletAddress}&tag=latest&apikey=${process.env.ETHERSCAN_API_KEY}`
+  );
+  const valueResponse = await valueFetch.json();
+  const valueData = valueResponse.result;
+  const value = (valueData / 10 ** 18) * priceData;
+
+  // Daily value
+  let dailyValue = 0;
+  if (lastWalletHistoryValue) {
+    dailyValue = value - lastWalletHistoryValue;
+  }
+
+  res.json({
+    allocation: allocation,
+    price: priceData,
+    dailyPrice: dailyPrice,
+    value: value,
+    dailyValue: dailyValue,
+  });
 });
 
 app.listen(port, () => {
