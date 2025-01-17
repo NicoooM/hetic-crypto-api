154ab4a96407198ac5329114c9a48a2202b2c145
Lucas Boucher <contact@lucasboucher.fr>
2025-01-17 09:32:44 +0100

    feat: helmet

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { prisma } from "lib/prisma";
 import { router } from "routes";
 import { verifyEnv } from "utils/verify-env";
 import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 const port = process.env.PORT;
@@ -10,6 +11,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1", router);
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
